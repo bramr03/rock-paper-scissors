4ab01744bd035ab7c575fb71f01374a50ca3ec00
Bram <bramrutgers14@gmail.com>
2022-11-14 00:32:21 +0100

    Added the logic for one game

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+const choices = ["rock", "paper", "scissors"]
+
 function getComputerChoice(){
-    return Math.floor(Math.random() * 3)
-    
+    let value = Math.floor(Math.random() * 3)
+    return choices[value];
 }
-let value =  getComputerChoice();
+
+function game(playerChoice,computerChoice){
+    if(playerChoice === computerChoice){
+        console.log("Draw")
+    }
+    else if(playerChoice === "rock"){
+        if(computerChoice === "scissors"){
+            console.log("You won!")
+        }
+        else{
+            console.log("You lost!")
+        }
+    }
+    else if(playerChoice === "paper"){
+        if(computerChoice === "rock"){
+            console.log("You won!")
+        }
+        else{
+            console.log("You lost!")
+        }
+        
+    }
+    else if(playerChoice === "scissors"){
+        if(computerChoice === "paper"){
+            console.log("You won!")
+        }
+        else{
+            console.log("You lost!")
+        }
+    }
+}
